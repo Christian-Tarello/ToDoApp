32cca73d88725cf084da09d42f71f564b9d52692
Christian Tarello <christian_tarello@outlook.com>
2023-08-05 22:25:11 -0500

    Add methods to Project class

--- src/modules/project.js
+++ src/modules/project.js
@@ -3,6 +3,32 @@ class Project {
         this.name = name;
         this.tasks = tasks;
     }
+
+    addTask(task) {
+        this.tasks.push(task)
+    }
+
+    insertTask(task, position) {
+        this.tasks.splice(position, 0, task)
+    }
+
+    removeTaskByName(taskName) {
+        const index = this.tasks.find((task) => task.name === taskName);
+        this.removeTaskById(index);
+    }
+
+    removeTaskByPosition(position) {
+        this.tasks.splice(position, 1);
+    }
+
+    getTaskByName(taskName) {
+        const index = this.tasks.find((task) => task.name === taskName);
+        return this.getTaskByIndex(index);
+    }
+
+    getTaskByPosition(position) {
+        return this.tasks(position);
+    }
 }
 
 /* In case it ever becomes a factory function instead */
