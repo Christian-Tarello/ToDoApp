1cef1c9c97035eeffe26852b8a3213f2aacb6399
Christian Tarello <christian_tarello@outlook.com>
2023-08-05 21:30:10 -0500

    Update todo module

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,10 +1,10 @@
-
-export class ToDo {
+class ToDo {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.isDone = false;
     }
 }
 
