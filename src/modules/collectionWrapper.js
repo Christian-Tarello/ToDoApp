4393a0f31b93ed472f39fea6c3b960459cc0f285
Christian Tarello <christian_tarello@outlook.com>
2023-10-09 05:39:39 -0500

    Add collection wrappers

---
+++ src/modules/collectionWrapper.js
@@ -0,0 +1,33 @@
+export default class CollectionWrapper {
+    constructor(items = []) {
+        this.items = [];
+
+        items.forEach((item) => {
+            this.addItem(item);
+        });
+    }
+
+    add(item) {
+        this.items.push(item);
+    }
+
+    remove(position) {
+        this.items.splice(position, 1);
+    }
+
+    insert(item, position) {
+        this.items.splice(position, 0, item);
+    }
+
+    replace(item, position) {
+        this.items.splice(position, 1, item);
+    }
+
+    get(position) {
+        return this.items[position]; 
+    }
+
+    items() {
+        return this.items;
+    }
+}
