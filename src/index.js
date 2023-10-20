ebe797a992e01cf6fd10b9a485d1c68bf35b5ff9
Christian Tarello <christian_tarello@outlook.com>
2023-10-19 19:07:43 -0500

    Add pop up layer to content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import AddTaskController from './modules/controllers/addTask/addTaskController';
 import ProjectController from './modules/controllers/addTask/projectController';
+import PopUpLayerController from './modules/controllers/addTask/popUpLayerController';
 import Project from './modules/models/project';
 import AddTaskView from './modules/views/addTaskView';
 import ProjectView from './modules/views/projectView';
+import PopUpLayerView from './modules/views/popUpLayerView';
 import './style.css';
 
 const contentHook = document.querySelector(".content");
+
+const popUpLayerController = new PopUpLayerController(new PopUpLayerView());
 
 const project = new Project("Test");
 const addTaskController = new AddTaskController(new AddTaskView());
 const projectController = new ProjectController(new ProjectView(addTaskController.view, project));
 
 contentHook.append(projectController.view.build());
+contentHook.append(popUpLayerController.view.build());
