da9ed738ddcadd9869c74c00355551e72c93e146
Christian Tarello <christian_tarello@outlook.com>
2023-11-07 18:11:37 -0600

    Section messages

--- src/modules/utils/messages.js
+++ src/modules/utils/messages.js
@@ -1,19 +1,39 @@
-const Messages = Object.freeze({
-    BUILT_TASK_INPUT: "BUILT_TASK_INPUT",
-    REMOVE_TASK_INPUT: "REMOVE TASK INPUT",
-    CREATE_TASK: "CREATE TASK",
-    NEW_TASK: "NEW TASK",
-    BUILT_TASK: "BUILT_TASK",
-    REMOVE_TASK: "REMOVE_TASK",
+const PopupMessages = Object.freeze({
     ADD_POP_UP: "ADD POP UP",
     ADD_SCROLL_LOCK: "ADD SCROLL LOCK",
     REMOVE_SCROLL_LOCK: "REMOVE_SCROLL_LOCK",
     ADD_CLICK_LOCK: "ADD CLICK LOCK",
     REMOVE_CLICK_LOCK: "REMOVE CLICK LOCK",
-    CREATE_PROJECT: "CREATE PROJECT",
+});
+
+const ModelMessages = Object.freeze({
+    NEW_TASK: "NEW TASK",
     NEW_PROJECT: "NEW PROJECT",
+});
+
+const ModelRequestMessages = Object.freeze({
+    CREATE_TASK: "CREATE TASK",
+    CREATE_PROJECT: "CREATE PROJECT",
+});
+
+const ElementMessages = Object.freeze({
+    BUILT_TASK: "BUILT_TASK",
     BUILT_PROJECT: "BUILT PROJECT",
+    BUILT_TASK_INPUT: "BUILT_TASK_INPUT",
     BUILT_EDIT_TASK_INPUT: "BUILT EDIT TASK INPUT",
+});
+
+const ElementRemovalMessages = Object.freeze({
+    REMOVE_TASK_INPUT: "REMOVE TASK INPUT",
+    REMOVE_TASK: "REMOVE_TASK",
+});
+
+const Messages = Object.freeze({
+    ...PopupMessages,
+    ...ModelMessages,
+    ...ModelRequestMessages,
+    ...ElementMessages,
+    ...ElementRemovalMessages
 });
 
 export default Messages;
