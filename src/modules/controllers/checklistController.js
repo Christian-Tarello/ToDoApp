3bd7b3984757f73e81885f1015bfc3b98de1aba3
Christian Tarello <christian_tarello@outlook.com>
2023-11-11 16:59:10 -0600

    Standardize update methods

--- src/modules/controllers/checklistController.js
+++ src/modules/controllers/checklistController.js
@@ -21,7 +21,7 @@ export default class ChecklistController {
     }
 
     update() {
-        this.view.updateState();
+        this.load();
     }
 
     addEmptyItem() {
--- src/modules/views/checklistView.js
+++ src/modules/views/checklistView.js
@@ -43,7 +43,7 @@ export default class ChecklistView {
     }
 
     updateState() {
-        this.controller.load();
+        this.controller.update();
     }
 
     replaceItems(elements) {
