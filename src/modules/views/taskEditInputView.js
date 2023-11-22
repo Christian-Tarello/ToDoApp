7b76afda3cabfba445e6cf5221ad5391707a6b9a
Christian Tarello <christian_tarello@outlook.com>
2023-11-22 06:39:54 -0600

    Refactor edit view

--- src/modules/views/taskEditInputView.js
+++ src/modules/views/taskEditInputView.js
@@ -17,45 +17,67 @@ export default class TaskEditInputView {
 
     createTemplate() {
         const element = this.createMainNode();
-        element.innerHTML = `
-            <ul>
-                <li>
-                    <label for="taskTitle">Title</label>
-                    <input type="text" id="taskTitle" name="${TaskFields.TITLE}" required>
-                </li>
-                <li>
-                    <label for="taskDescription">Description</label>
-                    <input type="text" id="taskDescription" name="${TaskFields.DESCRIPTION}" required>
-                </li>
-                <li>
-                    <label for="taskPriority">Priority</label>
-                    <select id="taskPriority" name="${TaskFields.PRIORITY}">
-                        <option value="${Priority.LOW}">Low</option>
-                        <option value="${Priority.MEDIUM}">Medium</option>
-                        <option value="${Priority.HIGH}">High</option>
-                        <option value="${Priority.UNSET}" selected>Unset</option>
-                    </select>
-                </li>
-                <li>
-                    <label for="taskDueDate">Due</label>
-                    <input type="date" id="taskDueDate" name="${TaskFields.DUE_DATE}">
-                </li>
-
-            </ul>
-            <button type="submit">Edit Task</button>
-            <button type="button">Cancel</button>             
-        `;
+        element.append(
+            this.createContentHook(),
+            this.createSubmitButton(),
+            this.createCancelButton()
+        );
         return element;
     }
 
-    setInteractions() {
-        const cancelButton = this.element.querySelector('button[type="button"]');
-        
+    setContent(data) {
+        this.contentHook.innerHTML = `
+        <ul>
+            <li>
+                <label for="taskTitle">Title</label>
+                <input type="text" id="taskTitle" name="${TaskFields.TITLE}" required>
+            </li>
+            <li>
+                <label for="taskDescription">Description</label>
+                <input type="text" id="taskDescription" name="${TaskFields.DESCRIPTION}" required>
+            </li>
+            <li>
+                <label for="taskPriority">Priority</label>
+                <select id="taskPriority" name="${TaskFields.PRIORITY}">
+                    <option value="${Priority.LOW}">Low</option>
+                    <option value="${Priority.MEDIUM}">Medium</option>
+                    <option value="${Priority.HIGH}">High</option>
+                    <option value="${Priority.UNSET}" selected>Unset</option>
+                </select>
+            </li>
+            <li>
+                <label for="taskDueDate">Due</label>
+                <input type="date" id="taskDueDate" name="${TaskFields.DUE_DATE}">
+            </li>
+        </ul>
+        `;
+    }
+
+    createContentHook() {
+        this.contentHook = document.createElement('ul');
+        return this.contentHook;
+    }
+
+    createSubmitButton() {
+        this.submitButton = document.createElement('button');
+        this.submitButton.setAttribute('type', 'submit');
+        this.submitButton.innerText = 'Edit Task';
+        return this.submitButton;
+    }
+
+    createCancelButton() {
+        this.cancelButton = document.createElement('button');
+        this.cancelButton.setAttribute('type', 'button');
+        this.cancelButton.innerText = 'Cancel';
+        return this.cancelButton;
+    }
+
+    setInteractions() {       
         this.element.addEventListener("submit", (e) => {
             e.preventDefault();
             this.controller.processTaskEditSubmission(e);
         });
-        cancelButton.addEventListener("click", () => {this.controller.remove()});
+        this.cancelButton.addEventListener("click", () => {this.controller.remove()});
     }
     
     build() {
