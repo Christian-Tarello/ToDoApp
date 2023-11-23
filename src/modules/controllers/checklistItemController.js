a12a7cb08adf084a98c25c2554d96e2681106dfc
Christian Tarello <christian_tarello@outlook.com>
2023-11-22 19:05:03 -0600

    Remove unused code

--- src/modules/controllers/checklistItemController.js
+++ src/modules/controllers/checklistItemController.js
@@ -8,23 +8,16 @@ export default class ChecklistItemController {
         this.view = view;
     }
 
-    updateView() {
-        this.view.setDescription(this.item.description);
-        this.view.setCheckboxState(this.item.isDone);
-    }
-
-    getData() {
-        return {
-            description: this.item.description,
-            isDone: this.item.isDone
-        }
-    }
-
     toggle() {
         this.item.toggleDone();
     }
 
     changeDescription(description) {
         this.item.description = description;
+    }
+
+    updateView() {
+        this.view.setDescription(this.item.description);
+        this.view.setCheckboxState(this.item.isDone);
     }
 }
