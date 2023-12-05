6f320e2b6081dbc0fd31b45f76cb4c37f7794f3f
Christian Tarello <christian_tarello@outlook.com>
2023-12-05 04:40:18 -0600

    Remove unused code

--- src/modules/controllers/projectCollectionController.js
+++ src/modules/controllers/projectCollectionController.js
@@ -34,6 +34,5 @@ export default class ProjectCollectionController {
     createEmptyProject() {
         const project = ModelFactory.createProject('');
         this.projectCollection.add(project);
-        this.updateView();
     }
 }
