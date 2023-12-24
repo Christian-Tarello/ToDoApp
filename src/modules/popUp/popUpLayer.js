f1b63d49f9fbd3c2a9692c915f616eda54b91ac4
Christian Tarello <christian_tarello@outlook.com>
2023-12-23 22:08:54 -0600

    Add missing line

--- src/modules/popUp/popUpLayer.js
+++ src/modules/popUp/popUpLayer.js
@@ -3,6 +3,7 @@ import createPopUp from "./popUp";
 export default class PopupLayer {
     constructor(contentHook) {
         this.contentHook = contentHook;
+        this.contentHook.classList.add('popUpLayer');
     }
 
     addPopUp(element, clickLock, scrollLock, backgroundColor, position) {
