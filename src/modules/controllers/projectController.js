5416853e275aedaef96587411b8b00a0ba99e9d2
Christian Tarello <christian_tarello@outlook.com>
2023-12-29 20:27:36 -0600

    Add styling to pop up form

--- src/modules/controllers/projectController.js
+++ src/modules/controllers/projectController.js
@@ -13,7 +13,7 @@ export default class ProjectController {
 
     addTaskInput() {
         const element = this.elementFactory.buildTaskInput(this.project);
-        this.popUpLayer.addPopUp(element, true, "#2d2d2dc2");
+        this.popUpLayer.addPopUp(element, true, "#80808080");
     }
 
     updateView() {
